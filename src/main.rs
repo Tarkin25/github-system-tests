d5485741be9dbecc233436dee4c7ee4dea411763
Tarkin25 <severin.weigold@gmx.ch>
2023-04-14 23:30:28 +0200

    add a simple axum server

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,25 @@
+use std::net::SocketAddr;
+
+use anyhow::Context;
+use axum::{routing::get, Router};
 use configuration::{init_logging, Configuration};
 
 mod configuration;
 
-fn main() -> anyhow::Result<()> {
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
     dotenv::dotenv().ok();
     let config = Configuration::new()?;
     init_logging(&config);
 
     tracing::debug!("{config:?}");
 
-    Ok(())
+    let app = Router::new().route("/", get(|| async { "it works" }));
+
+    let address = SocketAddr::from(([0, 0, 0, 0], config.server_port));
+
+    axum::Server::bind(&address)
+        .serve(app.into_make_service())
+        .await
+        .context("An error ocurred while running the axum server")
 }
